2ddf586657742b70abe873f5d8c3b6a5a8574e70
Loris Pozzobon <lpozzobon@vmware.com>
2023-09-22 17:41:31 +0200

    Install TAP

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ source util/text-formatting.sh
 source default-variables.sh
 
 clusterEssentialsFileName="tanzu-cluster-essentials-darwin-amd64-$clusterEssentialsVersion.tgz"
+tanzuImageRegistryHostname="registry.tanzu.vmware.com"
+tapImageRepository="tanzu-application-platform"
 
 main() {
   printHelp
@@ -18,9 +20,10 @@ main() {
       case $yn in
           [Yy]* )
             checkPreconditions
-            createCluster "$clusterName"
+            createCluster
             deployClusterEssentials
-
+            installTap
+            createDeveloperNamespace
             break;;
           [Nn]* ) break;;
           * ) printf -- "Please answer yes or no\n";;
@@ -34,7 +37,7 @@ printHelp() {
   printf -- "- Minikube >= 1.31.2\n"
   printf -- "- kubectl CLI >= 1.28.2\n"
   printf -- "- tanzu CLI >= 1.0.0\n"
-  printf -- "- Harbor registry with a user having write privileges\n"
+  printf -- "- An image registry with a user having write privileges\n"
   printf -- "- Enough hardware resources for a VM having %s virtual CPUs, %s MB of memory and %s of disk space\n" "$cpus" "$memory" "$diskSize"
   printf -- "- Archive %s in the same folder containing this script (you can download it from https://network.tanzu.vmware.com/products/tanzu-cluster-essentials/#/releases/1321952)\n\n" "$clusterEssentialsFileName"
 }
@@ -46,6 +49,12 @@ acquireSetupParams() {
   read -rp "Tanzu Network username: " tanzuNetworkUsername
   read -rsp "Tanzu Network password: " tanzuNetworkPassword
   printf -- "\n"
+  read -rp "Image registry hostname: " imageRegistryHostname
+  read -rp "Image registry username: " imageRegistryUsername
+  read -rsp "Image registry password: " imageRegistryPassword
+   printf -- "\n"
+  read -rsp "sudo password: " sudoPassword
+  printf -- "\n\n"
 }
 
 checkPreconditions() {
@@ -78,6 +87,11 @@ createCluster() {
 }
 
 deployClusterEssentials() {
+  export INSTALL_BUNDLE="registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:$clusterEssentialsInstallBundleSha"
+  export INSTALL_REGISTRY_HOSTNAME="registry.tanzu.vmware.com"
+  export INSTALL_REGISTRY_USERNAME=$tanzuNetworkUsername
+  export INSTALL_REGISTRY_PASSWORD=$tanzuNetworkPassword
+
   local clusterEssentialsInstallFolder="$HOME/tanzu-cluster-essentials"
   if [ -d "$clusterEssentialsInstallFolder" ]; then
     rm -rf "$clusterEssentialsInstallFolder";
@@ -85,13 +99,44 @@ deployClusterEssentials() {
 
   mkdir "$clusterEssentialsInstallFolder"
   tar -xvf "$clusterEssentialsFileName" -C "$clusterEssentialsInstallFolder"
-
-  export INSTALL_BUNDLE="registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:$clusterEssentialsInstallBundleSha"
-  export INSTALL_REGISTRY_HOSTNAME="registry.tanzu.vmware.com"
-  export INSTALL_REGISTRY_USERNAME=$tanzuNetworkUsername
-  export INSTALL_REGISTRY_PASSWORD=$tanzuNetworkPassword
-  cd "$clusterEssentialsInstallFolder"
+  pushd "$clusterEssentialsInstallFolder"
   ./install.sh --yes
+
+  echo "$sudoPassword" | sudo -S cp "$clusterEssentialsInstallFolder"/kapp /usr/local/bin/kapp
+  echo "$sudoPassword" | sudo -S cp "$clusterEssentialsInstallFolder"/imgpkg /usr/local/bin/imgpkg
+  printf -- "\n"
+  popd
+}
+
+installTap() {
+  kubectl create ns tap-install
+
+  tanzu secret registry add tap-registry \
+    --username "$tanzuNetworkUsername" \
+    --password "$tanzuNetworkPassword" \
+    --server "$tanzuImageRegistryHostname" \
+    --export-to-all-namespaces --yes --namespace tap-install
+
+  tanzu secret registry add registry-credentials \
+    --server   "$imageRegistryHostname" \
+    --username "$imageRegistryUsername" \
+    --password "$imageRegistryPassword" \
+    --namespace tap-install \
+    --export-to-all-namespaces \
+    --yes
+
+  tanzu package repository add tanzu-tap-repository \
+    --url "$tanzuImageRegistryHostname/${tapImageRepository}/tap-packages:$tapVersion" \
+    --namespace tap-install
+
+  tanzu package repository get tanzu-tap-repository --namespace tap-install
+
+  tanzu package install tap -p tap.tanzu.vmware.com -v "$tapVersion" --values-file tap-values.yaml -n tap-install --wait-timeout 120m0s
+}
+
+createDeveloperNamespace() {
+  kubectl create namespace dev
+  kubectl label namespaces dev apps.tanzu.vmware.com/tap-ns=""
 }
 
 main "$@"; exit
